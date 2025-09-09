266ba7306d52a38e81b814961257bb9990f7db9f
AndreDRI <andrewdrisostes@gmail.com>
2025-09-09 02:01:37 -0300

    (test): added fetch unit test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // Configuração Vite
 export default defineConfig({
   assetsInclude: ['**/*.html'],
+  plugins: [tsconfigPaths()],
+  test: {
+    environment: 'jsdom'
+  },
   server: {
-    port: 5173 // ou qualquer porta que quiser
+    port: 5173 
   }
 })
